6703ea38acaddae7447b7f22ed1eab6484756b7a
Chaim Muskal <monthlyschnitzel@gmail.com>
2019-09-02 20:30:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   if (!cardNumber) {
-    return ("Sorry we don't have a credit cardon file for you.")
+    return ("Sorry we don't have a credit card on file for you.")
   } else {
     var x = 0
     for (var i = 0; i < cart.length; ++i){
